da6f92468a282228557a88c838cf0796012c4d3a
arisolivia <aocarrol@gmail.com>
2020-12-02 12:29:42 -0500

    added classes to interact w/ database

---
+++ MavenPro1AnnaAris/src/main/java/com/revature/daoImpl/RequestDaoImpl.java
@@ -0,0 +1,5 @@
+package com.revature.daoImpl;
+
+public class RequestDaoImpl {
+
+}
---
+++ MavenPro1AnnaAris/src/main/java/com/revature/model/Request.java
@@ -0,0 +1,17 @@
+package com.revature.model;
+import java.util.List;
+
+public class Request {
+		
+	private int requestId;
+	private int employeeId;
+	private String eventDate;
+	private String eventTime;
+	private String eventLocation;
+	private String eventType;
+	private String eventDescription;
+	private double cost;
+	private String status;
+	private List<Document> additionalDocs;
+
+}

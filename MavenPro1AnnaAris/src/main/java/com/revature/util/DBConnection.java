33eebe12305857965bdbbf7099dd003e6b53c151
arisolivia <aocarrol@gmail.com>
2020-12-11 11:50:51 -0500

    resolved connection issue

--- MavenPro1AnnaAris/src/main/java/com/revature/util/DBConnection.java
+++ MavenPro1AnnaAris/src/main/java/com/revature/util/DBConnection.java
@@ -3,6 +3,7 @@ package com.revature.util;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -22,16 +23,25 @@ public class DBConnection {
 	}
 	public Connection getConnection() {
 		Connection connect = null;
-		Properties prop = new Properties();
-		try {
-			prop.load(new FileReader("database.properties"));
-			connect = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("password"));
+		//Properties prop = new Properties();
+		String resourceName = "database.properties"; // could also be a constant
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		Properties props = new Properties();
+		
+		try (InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+			props.load(resourceStream);
+			Class.forName("org.postgresql.Driver");
+			//prop.load(new FileReader("src/main/resources/database.properties"));
+			connect = DriverManager.getConnection(props.getProperty("host"), props.getProperty("user"), props.getProperty("password"));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 

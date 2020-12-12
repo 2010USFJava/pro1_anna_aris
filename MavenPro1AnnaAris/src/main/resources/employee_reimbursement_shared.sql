bb1031c8309cbf3ee8cd81a5af5fd105764b5ddc
mounkeygirl <mounkeygirl@yahoo.com>
2020-12-12 12:43:50 -0600

    updated table creation algorithm: courses, course_type, grade, presentation

--- MavenPro1AnnaAris/src/main/resources/employee_reimbursement_shared.sql
+++ MavenPro1AnnaAris/src/main/resources/employee_reimbursement_shared.sql
@@ -1,15 +1,22 @@
-create schema if not exists employee_reimbursement_shared authorization annacarl;
-set search_path to employee_reimbursement_shared; 
+--create schema if not exists employee_reimbursement_shared authorization annacarl;
+--set search_path to employee_reimbursement_shared; 
+
+
+create schema if not exists employee_reimbursement authorization annacarl;
+set search_path to employee_reimbursement; 
 
 drop table if exists employees cascade;
 drop table if exists departments cascade;
 drop table if exists logins cascade;
 drop table if exists request cascade;
-
-
+drop table if exists course_type cascade;
+drop table if exists course cascade;
+drop table if exists grade cascade;
+drop table if exists presentation cascade;
 
 drop sequence if exists emp_id_seq; 
 drop sequence if exists dep_id_seq; 
+drop sequence if exists course_id_seq; 
 
 create table employees (
 employee_id SERIAL,
@@ -60,6 +67,81 @@ start with 200;
 create sequence emp_id_seq
 increment by 1;
 
+create sequence course_id_seq
+increment by 1;
+
+
+
+--course tables
+CREATE TABLE course_type(
+course_type_id SERIAL,
+string name,
+reimbursement_percent integer,
+primary key(course_type_id)
+);
+
+
+--Hard coding this, since these numbers were set in instructions
+
+insert into course_type values(0,'University Course',80);
+insert into course_type values(1,'Seminar',60);
+insert into course_type values(2,'Certification Preperation Class',75);
+insert into course_type values(3,'Certification',100);
+insert into course_type values(4,'Techincal Training',90);
+insert into course_type values(5,'Other',30);
+
+CREATE TABLE course(
+course_id SERIAL,
+employee_id integer,
+course_type_id integer,
+name varchar,
+institution varchar,
+pass boolean,
+primary key(course_id)
+--foreign key(employee_id)
+--references employees(employee_id)
+--on delete cascade,
+--foreign key(course_type_id)
+--references course_type(employee_id)
+--on delete cascade
+);
+
+
+
+CREATE TABLE grade(
+course_id integer,
+letter_grade varchar(1),
+number_grade integer
+--foreign key(course_id)
+--references course_type(course_id)
+--on delete cascade
+);
+
+create table presentation(
+course_id integer,
+document_file text
+--foreign key(course_id)
+--references course_type(course_id)
+--on delete cascade
+);
+
+
+
+create view course_presentations as
+	select c.*,p.document_file
+	from course as c, presentation as p
+	inner join course
+	on course.course_id=p.course_id;
+
+create view course_grade as
+	select c.*,p.letter_grade, p.number_grade
+	from course as c, grade as p
+	inner join course
+	on course.course_id=p.course_id;
+
+select * from course_presentations;
+select * from course_grade;
+	
 --aris's table(s)
 CREATE TABLE request (
 	id serial,

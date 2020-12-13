c80073824b0b13da19adb855aefb88419964b95d
mounkeygirl <mounkeygirl@yahoo.com>
2020-12-13 13:24:07 -0600

    Fixed some log in problems. Changed updated CSS in aris pages to comments for her to opt to turn on if she wants. Will now be spinning off this into annav3

--- MavenPro1AnnaAris/src/main/webapp/myresources/js/employee.js
+++ MavenPro1AnnaAris/src/main/webapp/myresources/js/employee.js
@@ -3,32 +3,14 @@ window.onload=function(){
     console.log("in onload");
     // document.getElementById("digisubmit").addEventListener("click",getDigimon, false);
     document.getElementById("json_fname").innerHTML="HelloWorld";
-    getEmployee();
+    // getEmployee();
+    //console.log(currentEmp);
 
 
 }
 
 
-function getEmployee() {
-    console.log("in employee");
-    // let fName=document.getElementById("json_fname").value;
-    // Step 1
-    var xhr = new XMLHttpRequest();
-    // Step 2
-    xhr.onreadystatechange = function() {
-        console.log("in ORSC");
-        if (xhr.readyState==4&&xhr.status==200) {
-            console.log(xhr.responseText);
-            var employee = JSON.parse(xhr.responseText);
-            loadEmployee(employee);
-        }
-    }
-    // Step 3
-    xhr.open("POST", "employeeJsonString",true);
-    // Step 4
-    xhr.send();
 
-}
 
 function loadEmployee(employee){
     document.getElementById("json_fname").innerHTML="Goodbye Pie!";
@@ -39,6 +21,6 @@ function loadEmployee(employee){
 }
 
 function clickMe(){
-    document.getElementById("json_fname").innerHTML=employee[0];
+    document.getElementById("json_fname").innerHTML=currentEmp;
     // getEmployee();
 }
